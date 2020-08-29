b2adac48bc410447c4b9330378d3215c55a8f606
stephhsu <steph.c.hsu@gmail.com>
2020-08-28 23:59:28 -0600

    Updates

---
+++ precache-manifest.2d60f31b64f4a1fc3b2f2a38cfea9842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1915ca5f217fe8f59c05793dd656c9",
+    "url": "/musixplore-app/index.html"
+  },
+  {
+    "revision": "3ac74e8af104c74e3ed9",
+    "url": "/musixplore-app/static/css/main.37a3ac73.chunk.css"
+  },
+  {
+    "revision": "085965c7471668cc0b72",
+    "url": "/musixplore-app/static/js/2.c34cdbc7.chunk.js"
+  },
+  {
+    "revision": "17dffe90612e048fd9b4a769ac11b9cb",
+    "url": "/musixplore-app/static/js/2.c34cdbc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac74e8af104c74e3ed9",
+    "url": "/musixplore-app/static/js/main.55cc6af8.chunk.js"
+  },
+  {
+    "revision": "a4a9a1ec0dc6bb1720f8",
+    "url": "/musixplore-app/static/js/runtime-main.48f8078d.js"
+  }
+]);
